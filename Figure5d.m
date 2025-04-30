9d1b795540a962ac9a45af29b1f24bed536d49bb
Marino Pagan <marinopagan@gmail.com>
2025-04-30 12:54:54 +0100

    Update Figure5d.m

--- Figure5d.m
+++ Figure5d.m
@@ -7,6 +7,8 @@ close all
 
 rat_names={'P049','P055','P059','P095','P100','P101','P102'};
 
+
+%%% load results of logistic regression
 load data/logistic_regression_results_ephys_rats
 
 
@@ -15,36 +17,57 @@ for iii=1:length(rat_names)
 
     rat=rat_names{iii};
 
+    %%% compute slope of logistic regression
     slope_results2{iii}=compute_slope(logreg_results{iii});
 
     location(iii)=slope_results2{iii}.d_shap;
     frequency(iii)=slope_results2{iii}.f_shap;
 
+
+
+    %%% bootstrap to compute error bars for slope logistic regression
     clear locations frequencies
     for jjj=1:20
 
+        %%% load results of iteration jjj of bootstrap
         logreg_results_temp=logreg_results{iii};
         logreg_results_temp.thetadd=logreg_results_temp.thetadds(:,jjj);
         logreg_results_temp.thetadf=logreg_results_temp.thetadfs(:,jjj);
         logreg_results_temp.thetafd=logreg_results_temp.thetafds(:,jjj);
         logreg_results_temp.thetaff=logreg_results_temp.thetaffs(:,jjj);
 
-        slope_results_temp=compute_slope_errorbars(logreg_results_temp,slope_parameters);
+        %%% extract curves for relevant and irrelevant location and
+        %%% frequency evidence
+        d1=logreg_results_temp.thetadds(7:26,jjj);
+        d2=logreg_results_temp.thetadfs(7:26,jjj);
+        f1=logreg_results_temp.thetafds(7:26,jjj);
+        f2=logreg_results_temp.thetaffs(7:26,jjj);
+        x=logreg_results_temp.vecx(7:26);
 
-        locations(jjj)=slope_results_temp.d_shap;
-        frequencies(jjj)=slope_results_temp.f_shap;
+        %%% compute slope for iteration jjj
+        y=d1-d2;
+        y=y';
+        y=y/max(abs(y));
+        fit_result=polyfit(x,y,1);
+        locations(jjj)=fit_result(1);
+
+        y=f1-f2;
+        y=y';
+        y=y/max(abs(y));
+        fit_result=polyfit(x,y,1);
+        frequencies(jjj)=fit_result(1);
 
     end
 
+    %%% error bars are computed as standard deviation across bootstrap
+    %%% iterations
     location_se(iii)=std(locations);
     frequency_se(iii)=std(frequencies);
-
-
-
 
 end
 
 
+%%% sort rats by slope of location and frequency
 [~,ind_loc]=sort(location,'descend');
 [~,ind_freq]=sort(frequency,'descend');
 
@@ -61,50 +84,29 @@ ind_freq=indice_freq;
 
 
 
-logreg_parameters.type='ratio';
-
-logreg_parameters.lambda=200;
-
-logreg_parameters.min_perf=0.65;
-logreg_parameters.min_beta=0.6;
-logreg_parameters.min_trials=100;
-
-logreg_parameters.compute_bootstrap=0;
-logreg_parameters.bootstrap_reps=20;
-
-logreg_parameters.compute_halfsplit=0;
-
-
-
+%%% plot results logistic regression across rats and features
 h=figure;
 set(h,'Position',[220          57        1161         592]);
 
-
 for iii=1:length(rat_names)
-    iii
-    rat=rat_names{iii};
 
+    rat=rat_names{iii};
 
     subplot(4,7,ind_loc(iii))
 
     plot_logreg(logreg_results{iii},'location')
 
-
     title(rat)
-
 
     subplot(4,7,14+ind_freq(iii))
 
-
     plot_logreg(logreg_results{iii},'frequency')
 
-
     title(rat)
-
-
 end
 
 
+%%% plot neural pulse evoked population responses across rats and features
 
 for iii=1:length(rat_names)
     iii
@@ -128,11 +130,11 @@ end
 
 
 
+%%% scatterplot comparing neural data and behavioral logistic regression
 
-
+%%% define marker type for each rat
 shapes='.s^pvdh';
 sizes=[50 20 15 20 15 15 20]*1.3;
-
 
 
 figure;
@@ -165,6 +167,8 @@ set(gca,'TickDir','out')
 xlim([-2 1.15])
 ylim([-1 2.5])
 
+
+%%% compute values of correlation
 
 aaa=[location frequency];
 bbb=[loc_neur freq_neur];
